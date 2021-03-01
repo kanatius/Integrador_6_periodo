47b139ff388cbf985aa9d6e65b206e4bef09e638
kanatius <natanalmeidadelima@gmail.com>
2021-02-28 21:24:57 -0300

    função para inserir todas os distritos do Brasil no banco de dados

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,59 @@
 from unicodedata import normalize
+import requests
+from django.apps import apps
+
 
 def removeAccentsOfString(txt):
   return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
+
+
+def addRnCitiesFromIBGE():
+
+    Cidade = apps.get_model('imovel','Cidade')
+
+    estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"]
+
+    estados = {
+        'AC' : 'Acre',
+        'AL' : 'Alagoas',
+        'AP' : 'Amapá',
+        'AM' : 'Amazonas',
+        'BA' : 'Bahia',
+        'CE' : 'Ceará',
+        'DF' : 'Distrito Federal',
+        'ES' : 'Espirito Santo',
+        'GO' : 'Goiás',
+        'MA' : 'Maranhão',
+        'MS' : 'Mato Grosso do Sul',
+        'MT' : 'Mato Grosso',
+        'MG' : 'Minas Gerais',
+        'PA' : 'Pará',
+        'PB' : 'Paraíba',
+        'PR' : 'Paraná',
+        'PE' : 'Pernambuco',
+        'PI' : 'Piauí',
+        'RJ' : 'Rio de Janeiro',
+        'RN' : 'Rio Grande do Norte',
+        'RS' : 'Rio Grande do Sul',
+        'RO' : 'Rondônia',
+        'RR' : 'Roraima',
+        'SC' : 'Santa Catarina',
+        'SP' : 'São Paulo',
+        'SE' : 'Sergipe',
+        'TO' : 'Tocantins',
+    }
+
+
+    for sigla in estados:
+        cidades = requests.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + sigla + "/distritos").json()
+        
+        for cidade in cidades:
+
+            try:
+                Cidade.objects.get(nome=cidade["nome"], estado_sigla=sigla)    
+            except:
+                print("Saving: " + cidade["nome"])
+                cidade_model = Cidade(nome=cidade["nome"], estado=estados[sigla], estado_sigla=sigla)
+                cidade_model.save()
+    
+    
