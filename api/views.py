f882c0c5e03f3967c252127594d43803daca7821
kanatius <natanalmeidadelima@gmail.com>
2021-03-03 13:07:18 -0300

    autocomplete usando react funcionando

--- api/views.py
+++ api/views.py
@@ -7,10 +7,17 @@ from django.core import serializers
 # Create your views here.
 def get_cidades_by_estado(request, UF, city_name):
 
-    cidades = Cidade.objects.filter(estado_sigla__iexact=UF)
+    if request.method == "GET":
+        cidades = Cidade.objects.filter(estado_sigla__iexact=UF)
 
-    cidades = cidades.filter((Q(nome__icontains=city_name) | Q(nome_sem_acentos__icontains=city_name)))
+        cidades = cidades.filter((Q(nome__icontains=city_name) | Q(nome_sem_acentos__icontains=city_name)))
 
-    data = serializers.serialize("json", cidades)
+        data = ""
 
-    return HttpResponse(data, content_type='application/json')
+        if request.GET.get("limit"):
+            limit = int(request.GET.get("limit"))
+            data = serializers.serialize("json", cidades[:limit])
+        else:
+            data = serializers.serialize("json", cidades)
+
+        return HttpResponse(data, content_type='application/json')
--- core/static/js/index_react.js
+++ core/static/js/index_react.js
@@ -1,4 +1,4 @@
-const options = [{
+const estadoOptions = [{
         value: "AC",
         label: "Acre"
     },
@@ -108,22 +108,114 @@ const options = [{
     }
 ];
 
+var linkRoot = ""
+
 class SelectEstado extends React.Component {
-    
-    handleChange(e) {
-        console.log(e);
+
+    constructor(props){
+        super(props);
+        this.selectCidadeRef = props.selectCidadeRef;
     }
-    
+
+    onchangeEstado = (option) => {
+
+        linkRoot = "/api/" + option.value + "/cidades/a";
+        
+        var cidades = []
+        //variável para acessar o select
+        //dentro da função done o this muda
+        var this_ = this;
+        $.get(linkRoot)
+            .done(function (response) {
+                let options = [];
+
+                response.forEach(cidade => {
+                    options.push({
+                        label : cidade.fields.nome,
+                        value : cidade.fields.nome
+                    })
+                });
+                cidades = options; 
+        });
+
+        var maxTime = 3000;
+        var time_counter = 0;
+        var delay = 50;
+
+        var interval = setInterval(function(){
+            if (cidades.length > 0){
+                this_.selectCidadeRef.current.updateOptions(cidades);
+                clearInterval(interval);
+            }
+            if(time_counter > maxTime){
+                clearInterval(interval);
+            }
+            time_counter += delay;
+        }, delay);
+    }
+
     render() {
-        return <Select onChange={this.handleChange} options = {
-            options
+        return <Select onChange = {
+            this.onchangeEstado
+        }
+        options = {
+            estadoOptions
         }
         />;
     }
 }
 
+class SelectCidade extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name : "select cidade",
+            options : [{
+                value: "askldmlas",
+                label : "alksndkasn"
+            }]
+        }
+    }
+
+    printOptions = () => {
+        console.log(this.state.options);
+        console.log(this.state.name);
+    }
+
+    updateOptions(options){
+        this.setState({options : options});
+        console.log(this.state);
+    }
+
+    render() {
+        return <Select onChange={this.printOptions} options={this.state.options} />;
+    }
+}
+
+class SelectWrapper extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            cidadeOptions: [{
+                label : "TESTE CIDADE",
+                value : "asd"
+            }]
+        };
+        this.selectCidadeRef = React.createRef();
+    }
+
+    render() {
+        return (<div>
+            <SelectEstado selectCidadeRef={this.selectCidadeRef} />
+            <SelectCidade ref={this.selectCidadeRef}/>
+        </div>)
+    }
+}
+
 
 ReactDOM.render( <
-    SelectEstado / > ,
+    SelectWrapper / > ,
     document.getElementById('div-select')
 );
