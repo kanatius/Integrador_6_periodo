caa8174be18fd677dd1cf59926214c05fa6588eb
kanatius <natanalmeidadelima@gmail.com>
2021-03-04 00:09:56 -0300

    tela de busca versão inicial

--- core/static/js/index_react.js
+++ core/static/js/index_react.js
@@ -1,111 +1,111 @@
 const estadoOptions = [{
-        value: "AC",
-        label: "Acre"
-    },
-    {
-        value: "AL",
-        label: "Alagoas"
-    },
-    {
-        value: "AP",
-        label: "Amapá"
-    },
-    {
-        value: "AM",
-        label: "Amazonas"
-    },
-    {
-        value: "BA",
-        label: "Bahia"
-    },
-    {
-        value: "CE",
-        label: "Ceará"
-    },
-    {
-        value: "DF",
-        label: "Distrito Federal"
-    },
-    {
-        value: "ES",
-        label: "Espirito Santo"
-    },
-    {
-        value: "GO",
-        label: "Goiás"
-    },
-    {
-        value: "MA",
-        label: "Maranhão"
-    },
-    {
-        value: "MS",
-        label: "Mato Grosso do Sul"
-    },
-    {
-        value: "MT",
-        label: "Mato Grosso"
-    },
-    {
-        value: "MG",
-        label: "Minas Gerais"
-    },
-    {
-        value: "PA",
-        label: "Pará"
-    },
-    {
-        value: "PB",
-        label: "Paraíba"
-    },
-    {
-        value: "PR",
-        label: "Paraná"
-    },
-    {
-        value: "PE",
-        label: "Pernambuco"
-    },
-    {
-        value: "PI",
-        label: "Piauí"
-    },
-    {
-        value: "RJ",
-        label: "Rio de Janeiro"
-    },
-    {
-        value: "RN",
-        label: "Rio Grande do Norte"
-    },
-    {
-        value: "RS",
-        label: "Rio Grande do Sul"
-    },
-    {
-        value: "RO",
-        label: "Rondônia"
-    },
-    {
-        value: "RR",
-        label: "Roraima"
-    },
-    {
-        value: "SC",
-        label: "Santa Catarina"
-    },
-    {
-        value: "SP",
-        label: "São Paulo"
-    },
-    {
-        value: "SE",
-        label: "Sergipe"
-    },
-    {
-        value: "TO",
-        label: "Tocantins"
-    }
+    value: "AC",
+    label: "Acre"
+},
+{
+    value: "AL",
+    label: "Alagoas"
+},
+{
+    value: "AP",
+    label: "Amapá"
+},
+{
+    value: "AM",
+    label: "Amazonas"
+},
+{
+    value: "BA",
+    label: "Bahia"
+},
+{
+    value: "CE",
+    label: "Ceará"
+},
+{
+    value: "DF",
+    label: "Distrito Federal"
+},
+{
+    value: "ES",
+    label: "Espirito Santo"
+},
+{
+    value: "GO",
+    label: "Goiás"
+},
+{
+    value: "MA",
+    label: "Maranhão"
+},
+{
+    value: "MS",
+    label: "Mato Grosso do Sul"
+},
+{
+    value: "MT",
+    label: "Mato Grosso"
+},
+{
+    value: "MG",
+    label: "Minas Gerais"
+},
+{
+    value: "PA",
+    label: "Pará"
+},
+{
+    value: "PB",
+    label: "Paraíba"
+},
+{
+    value: "PR",
+    label: "Paraná"
+},
+{
+    value: "PE",
+    label: "Pernambuco"
+},
+{
+    value: "PI",
+    label: "Piauí"
+},
+{
+    value: "RJ",
+    label: "Rio de Janeiro"
+},
+{
+    value: "RN",
+    label: "Rio Grande do Norte"
+},
+{
+    value: "RS",
+    label: "Rio Grande do Sul"
+},
+{
+    value: "RO",
+    label: "Rondônia"
+},
+{
+    value: "RR",
+    label: "Roraima"
+},
+{
+    value: "SC",
+    label: "Santa Catarina"
+},
+{
+    value: "SP",
+    label: "São Paulo"
+},
+{
+    value: "SE",
+    label: "Sergipe"
+},
+{
+    value: "TO",
+    label: "Tocantins"
+}
 ];
 
 var linkRoot = ""
@@ -131,7 +131,7 @@ class SelectEstado extends React.Component {
         super(props);
         this.selectCidadeRef = props.selectCidadeRef;
         this.state = {
-            idSelect : props.idSelectEstado
+            idSelect: props.idSelectEstado
         }
     }
 
@@ -164,12 +164,13 @@ class SelectEstado extends React.Component {
     }
 
     render() {
-        return <Select 
-        id= {this.state.idSelect}
-        onChange = {this.onchangeEstado}
-        options = {
-            estadoOptions
-        }
+        return <Select
+            id={this.state.idSelect}
+            onChange={this.onchangeEstado}
+            options={
+                estadoOptions
+            }
+            placeholder={"Digite o nome do estado"}
         />;
     }
 }
@@ -181,7 +182,7 @@ class SelectCidade extends React.Component {
         this.state = {
             options: [],
             estado: "",
-            idSelect : props.idSelectCidade
+            idSelect: props.idSelectCidade
         }
     }
 
@@ -193,8 +194,8 @@ class SelectCidade extends React.Component {
         //variável para acessar o select
         //dentro da função done o this muda
         var this_ = this;
-        
-        if (inputText.length < 2 || this.state.estado == ""){
+
+        if (inputText.length < 2 || this.state.estado == "") {
             return
         }
         console.log(linkRoot);
@@ -208,7 +209,7 @@ class SelectCidade extends React.Component {
             if (cidades.length > 0) {
                 this_.updateOptions(cidades);
                 time_counter = 0;
-                clearInterval(interval); 
+                clearInterval(interval);
             }
             if (time_counter > maxTime) {
                 time_counter = 0;
@@ -217,12 +218,11 @@ class SelectCidade extends React.Component {
             }
             time_counter += delay;
         }, delay);
-      };
+    };
 
-    // printOptions = () => {
-    //     console.log(this.state.options);
-    //     console.log(this.state.name);
-    // }
+    onChange(option) {
+        $("#input-cidade").val(option.label);
+    }
 
     updateOptions(options) {
         this.setState({
@@ -230,50 +230,63 @@ class SelectCidade extends React.Component {
         });
     }
 
-    updateEstado(estado){
+    updateEstado(estado) {
         this.state.estado = estado;
+        $("#input-estado").val(estado);
     }
 
     render() {
-        return <Select 
-        id= {this.state.idSelect}
-        // onChange = {this.printOptions} 
-        onInputChange={this.onInputChange}
-        options = {this.state.options}
+        return <Select
+            id={this.state.idSelect}
+            onInputChange={this.onInputChange}
+            onChange={this.onChange}
+            options={this.state.options}
+            placeholder={"Digite o nome da cidade"}
+            noOptionsMessage={() => 'Selecione o estado e digite o nome da cidade'}
         />;
     }
 }
 
 class SelectWrapper extends React.Component {
 
-        constructor(props) {
-            super(props);
-            this.state = {
-                idSelectCidade : props.idSelectCidade,
-                idSelectEstado : props.idSelectEstado
-            };
-            this.selectCidadeRef = React.createRef();
-        }
+    constructor(props) {
+        super(props);
+        this.state = {
+            idSelectCidade: props.idSelectCidade,
+            idSelectEstado: props.idSelectEstado
+        };
+        this.selectCidadeRef = React.createRef();
+    }
 
-        render() {
-            return ( <div>
-                <label>Selecione o Estado</label>
-                <SelectEstado idSelectEstado={this.props.idSelectEstado} selectCidadeRef = {
-                    this.selectCidadeRef
-                }
-                />
-                <label>Selecione a cidade</label>
-                <SelectCidade idSelectCidade={this.props.idSelectCidade} ref = {
-                    this.selectCidadeRef
-                }
-                /> </div>)
-            }
-        }
+    render() {
+        return (
+            <form>
+                <input type="text" hidden name="cidade" id="input-cidade" />
+                <input type="text" hidden name="estado" id="input-estado" />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-12 col-lg-8">
+                            <label>Selecione a cidade</label>
+                            <SelectCidade idSelectCidade={this.props.idSelectCidade} ref={this.selectCidadeRef} />
+                        </div>
+                        <div className="col-sm-12 col-md-12 col-lg-4">
+                            <label>Selecione o Estado</label>
+                            <SelectEstado idSelectEstado={this.props.idSelectEstado} selectCidadeRef={this.selectCidadeRef} />
+                        </div>
+                    </div>
+                </div>
+                <div className="text-end">
+                    <button className="btn" id="btn-enviar" type="submit">Procurar Imóveis</button>
+                </div>
+            </form>
+        )
+    }
+}
 
 
-ReactDOM.render( <SelectWrapper  
+ReactDOM.render(<SelectWrapper
     idSelectCidade={"select-cidade"}
-    idSelectEstado={"select-estado"}  
-    /> ,
+    idSelectEstado={"select-estado"}
+/>,
     document.getElementById('div-select')
 );
