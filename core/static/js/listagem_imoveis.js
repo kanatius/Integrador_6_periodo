cf6e9700be7b68ffd0ab626eb484e554cb874323
kanatius <natanalmeidadelima@gmail.com>
2021-03-07 17:33:39 -0300

    Listagem versão inicial

--- core/static/js/listagem_imoveis.js
+++ core/static/js/listagem_imoveis.js
@@ -1,83 +1,45 @@
 var nextId = 50;
 const imoveisTeste = [
-    {
-        id: 1,
-        tipo: "Apartamento",
-        descricao: "Em ótimo estado",
-        ponto_de_referencia: "Perto da UERN",
-        logradouro: "5044 Brooke Knolls",
-        imagemURL: "/static/casa.jpg"
-    },
-    {
-        id: 2,
-        tipo: "Apartamento",
-        descricao: "Em ótimo estado",
-        ponto_de_referencia: "Perto da UERN",
-        logradouro: "Santa Rita",
-        imagemURL: "/static/casa2.jpg"
-    },
-    {
-        id: 3,
-        tipo: "Casa",
-        descricao: "Em ótimo estado",
-        ponto_de_referencia: "Perto da UERN",
-        logradouro: "Rua Boa Vista",
-        imagemURL: "/static/casa.jpg"
-    },
-    {
-        id: 4,
-        tipo: "Apartamento",
-        descricao: "Em ótimo estado",
-        ponto_de_referencia: "Perto da UERN",
-        logradouro: "4332 Leannon Motorway",
-        imagemURL: "/static/casa2.jpg"
-    }
-    // {
-    //     id: 5,
-    //     tipo: "Casa",
-    //     descricao: "Em ótimo estado",
-    //     ponto_de_referencia: "Perto da UERN",
-    //     logradouro: "5044 Brooke Knolls",
-    //     imagemURL: "/static/casa.jpg"
-    // },
-    // {
-    //     id: 6,
-    //     tipo: "Casa",
-    //     descricao: "Em ótimo estado",
-    //     ponto_de_referencia: "Perto da UERN",
-    //     logradouro: "5044 Brooke Knolls",
-    //     imagemURL: "/static/casa.jpg"
-    // },
-    // {
-    //     id: 7,
-    //     tipo: "Apartamento",
-    //     descricao: "Em ótimo estado",
-    //     ponto_de_referencia: "Perto da UERN",
-    //     logradouro: "5044 Brooke Knolls",
-    //     imagemURL: "/static/casa2.jpg"
-    // }
+    
 ]
+
+//link root para a chamada para carregar os imóveis disponíveis na cidade
+var linkRoot = "/api/imoveis/" + document.getElementById("estado").value.toString() + "/" + document.getElementById("cidade").value.toString()
+
+//carrega os primeiros dados da página
+$(window).ready(function(){
+    document.getElementById("carregar-mais").click();
+});
+
 
 class ListaImoveis extends React.Component {
 
     constructor(props) {
         super(props);
+        
         this.state = {
-            cards: imoveisTeste
-        }
+            imoveis: [],
+            carregando : false,
+            qtdImoveisPorVez : 1,
+            carregouTodos : false
+        };
     }
 
-    getCard = (card) => {
+    getCard = (imovel) => {
 
+        console.log(imovel);
+
+        let urlImagem = (imovel.imagens.length == 0) ? "" : imovel.imagens[0].link;
+        console.log(urlImagem);
         return (
-            <div key={card.id} className="card col-lg-7">
+            <div key={imovel.id} className="card col-lg-7">
                 <div className="card-img">
-                    <img src={card.imagemURL} className="card-img-top" alt="..." />
+                    <img src={urlImagem} className="card-img-top" alt="..." />
                 </div>
                 <div className="card-body">
-                    <h5 className="card-title"><b>{card.tipo}</b></h5>
-                    <p className="card-text"><b>{card.logradouro}</b></p>
-                    <p className="card-text">{card.descricao}</p>
+                    <h5 className="card-title"><b>{imovel.tipo}</b></h5>
+                    <p className="card-text"><b>{imovel.endereco.logradouro}, Nº {imovel.endereco.numero} - Bairro: {imovel.endereco.bairro}</b></p>
+                    <p className="card-text">{imovel.descricao}</p>
                 </div>
                 <div className="card-footer">
                     <small className="text-muted"><a href="#">Detalhes</a></small>
@@ -86,25 +48,42 @@ class ListaImoveis extends React.Component {
     }
 
     carregarMais() {
-        let imgURL = (nextId % 2 == 0) ? "/static/casa.jpg" : "/static/casa2.jpg";
-        this.state.cards.push({
-            id: nextId++,
-            tipo: "Apartamento",
-            descricao: "Em ótimo estado",
-            ponto_de_referencia: "Perto da UERN",
-            logradouro: "Santa Rita",
-            imagemURL: imgURL
-        });
 
-        this.setState({
-            cards: this.state.cards
+        //proibe que se faça mais de uma requisição por vez, pra evitar carregar dados iguais
+        //Se carregou todos os imóveis não solicita mais
+        if (this.state.carregando || this.state.carregouTodos){
+            return
+        }
+        //modifica o status de chamada pra true
+        this.state.carregando = true;
+
+        //backup para acesso dentro da chamada ajax
+        let _this = this;
+
+        //offset sempre será o valor dos imóveis já carregados
+        let offset = this.state.imoveis.length
+
+        $.get(linkRoot + "?offset="+ offset +"&limit=" + this.state.qtdImoveisPorVez).done(function(imoveis){
+            
+            //adiciona os imóveis carregados
+            _this.state.imoveis.push(...imoveis);
+
+            if(imoveis.length < _this.state.qtdImoveisPorVez){
+                //se a quantidade de imoveis retornados for menor que a quantidade solicitada
+                //define como true
+                _this.state.carregouTodos = true;
+            }
+
+            _this.setState({imoveis: _this.state.imoveis});
+
+            _this.state.carregando = false;
         });
     }
 
     render() {
         return (
             <div id="div-cards" className="row">
-                {this.state.cards.map(this.getCard)}
+                {this.state.imoveis.map(this.getCard)}
                 <button id="carregar-mais" className="btn" hidden onClick={this.carregarMais.bind(this)}>Carregar mais</button>
             </div>
         );
@@ -114,7 +93,7 @@ class ListaImoveis extends React.Component {
 var listaIMoveis = React.createElement(ListaImoveis);
 
 $(window).scroll(function () {
-    if ($(window).scrollTop() >= $(document).height() - $(window).height() - 400) {
+    if ($(window).scrollTop() >= $(document).height() - $(window).height() - 150) {
         document.getElementById("carregar-mais").click();
     }
 });
