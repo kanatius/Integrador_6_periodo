1f68d051a7e684873969633aca5b2b809243d4d4
kanatius <natanalmeidadelima@gmail.com>
2021-03-10 19:09:13 -0300

    Tela - seção de enviar e-mail para o proprietário, versão inicial

--- core/static/js/imovel_detalhes.js
+++ core/static/js/imovel_detalhes.js
@@ -17,6 +17,33 @@ function handleImgList(){
     } 
 }
 
+var usuarioNome = document.getElementById("usuario-nome").value;
+var usuarioTelefone = document.getElementById("usuario-telefone").value;
+var usuarioEmail = document.getElementById("usuario-email").value;
+
+function mensagemTipo2(){
+    if (usuarioEmail == ""){
+        return alert("Função disponível somente para usuários logados");
+    }
+    let mens = "Olá :D, bom dia! Me chamo " + usuarioNome +", e gostaria de mais informações sobre seu imóvel.\n\nPor favor, entrar em contato comigo: " + usuarioTelefone + " - " + usuarioEmail;
+    document.getElementById("textarea-mens").value = mens;
+}
+function mensagemTipo1(){
+    if (usuarioEmail == ""){
+        return alert("Função disponível somente para usuários logados");
+    }
+    let mens = "Olá, me chamo " + usuarioNome +" e me interessei pelo seu imóvel. Estou entrando em contato em breve!";
+    document.getElementById("textarea-mens").value = mens;
+}
+
+function mensagemTipo3(){
+    if (usuarioEmail == ""){
+        return alert("Função disponível somente para usuários logados");
+    }
+    let mens = "Bom dia! Meu nome é " + usuarioNome +", gostei bastante do seu imóvel, poderia entrar em contato comigo para mais informações?\n\nContato: " + usuarioTelefone + " - " + usuarioEmail;
+    document.getElementById("textarea-mens").value = mens;
+}
+
 $(window).ready(function(){
     handleImgList();
     window.onresize = handleImgList;
