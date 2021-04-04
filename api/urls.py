b09f6aa383fbbd5c3ab59b792ad631526ece63be
kanatius <natanalmeidadelima@gmail.com>
2021-04-04 10:10:50 -0300

    documentação api com drf_yasg

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,35 @@
 from django.urls import path, include
 from api import views as api_views
-
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+# from rest_framework_simplejwt import views as jwt_views
 
 app_name = "api"
 
 
 urlpatterns = [
-    path("<str:UF>/cidades/<str:city_name>", api_views.get_cidades_by_estado, name="cidade_by_estado"),
-    path("imoveis/<str:UF>/<str:city_name>", api_views.get_imoveis, name="getImoveis")
+    path("<str:UF>/cidades/<str:city_name>", api_views.get_cidades_by_estado, name="cidadeByEstado"),
+    path("imoveis/<str:UF>/<str:city_name>", api_views.get_imoveis, name="getImoveis"),
+    path("meusImoveis", api_views.meusImoveis, name="meusImoveis"),
+    path("imovel/<int:id>", api_views.getImovel, name="imvoelById")
+]
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API ALUGAAQUI",
+      default_version='v1',
+      description="Documentação Aluga Aqui",
+    #   terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="alugaaqui@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
--- api/views.py
+++ api/views.py
@@ -4,12 +4,22 @@ from django.http import HttpResponse
 from django.db.models import Q
 from django.core import serializers
 from .serializers import ImovelSerializer, CidadeSerializer
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view, parser_classes, permission_classes
 import json
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
-# Create your views here.
+offset = openapi.Parameter('offset', openapi.IN_QUERY, default=0, description="", type=openapi.TYPE_INTEGER)
+limit= openapi.Parameter('limit', openapi.IN_QUERY, default=0, description="Limite de impressões retornadas", type=openapi.TYPE_INTEGER)
+
+
+
+@swagger_auto_schema(method='GET',  manual_parameters=[offset, limit], responses={ 200 : CidadeSerializer})
+@api_view(["GET"])
 def get_cidades_by_estado(request, UF, city_name):
 
     if request.method == "GET":
@@ -17,6 +27,10 @@ def get_cidades_by_estado(request, UF, city_name):
 
         cidades = cidades.filter((Q(nome__icontains=city_name) | Q(nome_sem_acentos__icontains=city_name)))
 
+        if request.GET.get("offset"):
+            offset = int(request.GET.get("offset"))
+            cidades = cidades[offset:]
+    
         if request.GET.get("limit"):
             limit = int(request.GET.get("limit"))
             cidades = cidades[:limit]
@@ -26,6 +40,8 @@ def get_cidades_by_estado(request, UF, city_name):
         return HttpResponse(data, content_type='application/json')
 
 
+@swagger_auto_schema(method='GET', manual_parameters=[offset, limit], responses={ 200 : ImovelSerializer})
+@api_view(["GET"])
 def get_imoveis(request, UF, city_name):
 
     imoveis = Imovel.objects.filter(endereco__cidade__nome=city_name, endereco__cidade__estado_sigla=UF)
@@ -45,3 +61,43 @@ def get_imoveis(request, UF, city_name):
     data = json.dumps(data)
 
     return HttpResponse(data, content_type='application/json')
+
+@swagger_auto_schema(method='GET',  manual_parameters=[offset, limit], responses={ 200 : ImovelSerializer})
+@api_view(["GET"])
+@permission_classes([permissions.IsAuthenticated])
+def meusImoveis(request):
+
+    imoveis = Imovel.objects.filter(proprietario=request.user)
+
+
+    if request.GET.get("offset"):
+        offset = int(request.GET.get("offset"))
+        imoveis = imoveis[offset:]
+    
+    if request.GET.get("limit"):
+        limit = int(request.GET.get("limit"))
+        if limit > 0:
+            imoveis = imoveis[:limit]
+
+
+    data = ImovelSerializer(imoveis, many=True).data
+
+    data = json.dumps(data)
+
+    return HttpResponse(data, content_type='application/json')
+
+@swagger_auto_schema(method='GET', responses={ 200 : ImovelSerializer})
+@api_view(["GET"])
+def getImovel(request, id):
+
+    try:
+        imovel = Imovel.objects.get(id=id)
+        data = ImovelSerializer(imovel, many=False).data
+
+        data = json.dumps(data)
+
+        return HttpResponse(data, content_type='application/json')
+
+    except:
+        data = json.dumps(None)
+        return HttpResponse(data, content_type='application/json')
